af897cd829893ec2e0c6bbb8a9a2c6607cf61cf8
weiss0106 <franc0106@163.com>
2025-09-26 22:47:06 +0200

    Add client and server interfaces for ASketch

---
+++ client/asketch.go
@@ -0,0 +1,102 @@
+package client
+
+import (
+	"fmt"
+	"reflect"
+
+	pb "github.com/bruhng/distributed-sketching/proto"
+	"github.com/bruhng/distributed-sketching/shared"
+	"github.com/bruhng/distributed-sketching/sketches/asketch"
+	"github.com/bruhng/distributed-sketching/stream"
+)
+
+func ASketchClient[T shared.Number](mergeAfter int, dataStream stream.Stream[T], addr string, startConnection connectionStarter) {
+	var reconAttempt *int = new(int)
+	*reconAttempt = 0
+	c, conn, err := startConnection(addr)
+	if err != nil {
+		fmt.Printf("Connection failed with error: %v\n", err)
+		panic("could not start connection")
+	}
+	sketch := asketch.NewASketch[T](shared.ASketchSeed, shared.ASketchWidth, shared.ASketchDepth, shared.ASketchSlots)
+	i := 0
+	for data := range dataStream.Data {
+		sketch.Add(data)
+		i++
+
+		if i%mergeAfter == 0 {
+			protoSketch := ConvertToProtoASketch(sketch)
+
+			MakeRequest(protoSketch, addr, c.MergeASketch, conn, &c, startConnection, reconAttempt)
+			sketch = asketch.NewASketch[T](shared.ASketchSeed, shared.ASketchWidth, shared.ASketchDepth, shared.ASketchSlots)
+		}
+	}
+	if conn != nil {
+		conn.Close()
+	}
+	blackhole = sketch
+}
+
+func GetASketch[T shared.Number](mergeAfter int, dataStream stream.Stream[T]) *pb.ASketch {
+	sketch := asketch.NewASketch[T](shared.ASketchSeed, shared.ASketchWidth, shared.ASketchDepth, shared.ASketchSlots)
+	i := 0
+	for data := range dataStream.Data {
+		sketch.Add(data)
+		i++
+
+		if i%mergeAfter == 0 {
+			return ConvertToProtoASketch(sketch)
+		}
+	}
+	return nil
+}
+
+func ConvertToProtoASketch[T shared.Number](sketch *asketch.ASketch[T]) *pb.ASketch {
+	t := fmt.Sprintf("%T", *new(T))
+
+	// Get snapshot of the sketch
+	filter, rows, seeds := sketch.Snapshot()
+
+	protoASketch := &pb.ASketch{Type: t}
+
+	// Convert filter entries
+	for _, slot := range filter {
+		var protoValue *pb.NumericValue
+
+		if reflect.ValueOf(slot.Item).Kind() == reflect.Int {
+			protoValue = &pb.NumericValue{
+				Value: &pb.NumericValue_IntVal{IntVal: int64(slot.Item)},
+				Type:  "int",
+			}
+		} else {
+			protoValue = &pb.NumericValue{
+				Value: &pb.NumericValue_FloatVal{FloatVal: float64(slot.Item)},
+				Type:  "float64",
+			}
+		}
+
+		protoEntry := &pb.ASketchFilterEntry{
+			Item: protoValue,
+			Old:  int64(slot.Old),
+			New:  int64(slot.New),
+		}
+
+		protoASketch.Filter = append(protoASketch.Filter, protoEntry)
+	}
+
+	// Convert CountMin data
+	protoCountMin := &pb.CountMin{}
+
+	for _, row := range rows {
+		protoRow := &pb.IntRow{}
+		for _, val := range row {
+			protoRow.Val = append(protoRow.Val, int64(val))
+		}
+		protoCountMin.Rows = append(protoCountMin.Rows, protoRow)
+	}
+
+	protoCountMin.Seeds = append(protoCountMin.Seeds, seeds...)
+	protoASketch.CountMin = protoCountMin
+
+	return protoASketch
+}
